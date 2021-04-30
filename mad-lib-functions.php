f873830920303066ae1b2e2d1303909c1e8388f5
ivobul <24499940+ivobul@users.noreply.github.com>
2021-04-30 08:23:03 -0400

    Add files via upload

---
+++ mad-lib-functions.php
@@ -0,0 +1,15 @@
+<?php
+  function generateStory($singular_noun, $verb, $color, $distance_unit) {
+    $story = "\nThe ${singular_noun}s are lovely, ${color}, and deep.
+      \nBut I have promises to keep,
+      \nAnd ${distance_unit} to go before I ${verb},
+      \nAnd ${distance_unit} to go before I ${verb}.\n";
+
+	return $story;
+  }
+
+  echo generateStory("dog", "eat", "purple", "yards");
+  echo str_repeat("-", 30);
+  echo generateStory("car", "cook", "vermilion", "metres");
+  echo str_repeat("-", 30);
+  echo generateStory("empty void", "speak", "beige", "feets");
